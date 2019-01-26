6f037ce471ea0e524d57af2120e66b55364ac831
Arielvdd <43513265+Arielvdd@users.noreply.github.com>
2019-01-26 11:38:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ bot.login(config.token);
 function clear() {
     console.clear();
     console.log(figlet.textSync("MassDM v3.2.0").green);
-    console.log("\n\nMass DM bot for Discord. Credit to Gringo(Scammer ALT)!\nSends DMs to selected members of guild.");
-    console.log(`\nForked and improved by Alex.\n Random send time set @ 0.01-${config.wait}s`);
+    console.log("\n\nMass DM bot for Discord. Credit to Ariel!\nSends DMs to selected members of guild.");
+    console.log(`\nForked and improved by Ariel.\n Random send time set @ 0.01-${config.wait}s`);
     console.log(`Type ${config.prefix}help in a chat.\n\n`);
 }
